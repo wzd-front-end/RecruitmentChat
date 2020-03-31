e4510a1032280ed08ab1083a4ce47a3b955286b1
wuzd <429023964@qq.com>
2020-03-31 11:55:41 +0800

    add：增加测试用例

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import {Provider} from 'react-redux'
 import {BrowserRouter} from "react-router-dom"
 import App from './app'
 import reducers from './reducer'
+// import Test from './component/test/test'
 
 import './config'
 import './index.css'
@@ -19,6 +20,7 @@ const store = createStore(reducers, compose(
 //这里有个自动跳转得组件，是该课程设计的知识点，
 //在路由组件执行前先进行一个身份登陆得检查，即能够达到检查，又在同时放于路由相关模块，便于查看，跟重定向Redirect有异曲同工之妙
 ReactDOM.render(
+  // (<Test></Test>),
   (<Provider store={store}>
     <BrowserRouter>
       <App></App>
